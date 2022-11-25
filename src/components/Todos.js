8a0ff6ef1caef4bb3f223fcf7795478e608a5e17
Lucas García <lucas@codeccoop.org>
2022-11-25 10:47:22 +0100

    Add webpack dev server

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,13 @@
 import List from "./List";
 
 function Todos(todos) {
-  return "<h1>TODO List</h1>" + '<input id="input" type="text" />' + "<hr>" + List(todos);
+  return (
+    "<h1>TODO List</h1>" +
+    '<label id="newTaskLabel">Add a task</label><br/>' +
+    '<input id="input" aria-labeledby="newTaskLabel" type="text" />' +
+    "<hr>" +
+    List(todos)
+  );
 }
 
 export default Todos;
